daab7d5eb366fc2e6815af75517d4cbc94afba38
kunj-cmyk <kunj@savitarainfotel.in>
2025-05-22 16:49:40 +0530

    added a filter as a component

--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -51,38 +51,7 @@ new class extends Component
     </nav>
 
     <div id="navPlaceholder" class="placeholder d-none"></div>
-
     <!-- Filter Bar -->
-    <div id="filterBar" class="filter-bar d-flex align-items-center  justify-content-xl-center">
-      <button class="btn btn-outline"><i class="bi bi-chevron-left"></i></button>
-      <button class="btn btn-outline"><i class="bi bi-chevron-right"></i></button>
-      <button class="btn btn-outline">May 9, 2025 <i class="bi bi-calendar-plus ms-2"></i></button>
-      <button class="btn btn-outline dropdown-toggle">YoY 5 days ago</button>
-      <button class="btn btn-outline"><i class="bi bi-tags me-2"></i> Upsell</button>
-      <button class="btn btn-outline"><i class="bi bi-sliders2-vertical me-2"></i> Channels</button>
-      <span class=" ">1 Night</span>
-      <span class="mx-3 ">2 Guest</span>
-      <span class=" me-3">Any Room</span>
-      <div class="dropdown">
-        <button class="btn btn-outline dropdown-toggle" data-bs-toggle="dropdown">Primary Compete</button>
-        <ul class="dropdown-menu">
-          <li><a class="dropdown-item" href="#">Primary Compete</a></li>
-          <li><a class="dropdown-item" href="#">Secondary Compete</a></li>
-        </ul>
-      </div>
-    </div>
-
-    <div id="filterPlaceholder" class="placeholder-filter d-none"></div>
-    <!-- Offcanvas Sidebar for Mobile Menu -->
-    <div class="offcanvas offcanvas-end" tabindex="-1" id="mobileMenu">
-      <div class="offcanvas-header">
-        <h5 class="offcanvas-title">Menu</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="offcanvas"></button>
-      </div>
-      <div class="offcanvas-body">
-        <a href="#" class="d-block mb-2">Overview</a>
-        <a href="#" class="d-block mb-2">Rates</a>
-      </div>
-    </div>
+    < x-filter />
 
   </header>
